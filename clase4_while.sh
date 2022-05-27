f4a112c71c8840beb4b18f8142fbce1b1eb8ab09
SaborCanela <thomas.garzon@est.ikiam.edu.ec>
2022-05-26 19:25:59 -0500

    Clase4

---
+++ clase4_while.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dividendo=$1
+divisor=$2
+cambio=$3
+while [ "${dividendo}" -le "100" ]
+do
+    x=$dividendo/$divisor
+    echo "la division es $dividendo sobre $divisor es $x"
+    dividendo=$(( dividendo+$cambio ))
+done
